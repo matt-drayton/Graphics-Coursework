287bbe3b7404146cfb34e5bec9d361db6fa65830
Matt Drayton <matthewdrayton@outlook.com>
2020-12-08 22:39:55 +0000

    Final push

--- FurModel.py
+++ FurModel.py
@@ -10,32 +10,31 @@ from BaseModel import BaseModel
 
 class FurModel(BaseModel):
     """
-    Base class for all models, implementing the basic draw function for triangular meshes.
-    Inherit from this to create new models.
+    Fur model. Can be instansiated with the vertices, normals, and indices from any model. Will create a separate mesh that adds fur. 
     """
 
-    def __init__(self, scene, vertices, normals, indices, fur_length=0.1, fur_angle=30, angle1=30, angle2=30, fur_density=0, M=poseMatrix(), material=None, primitive=GL_LINES, visible=True,):
+    def __init__(self, scene, vertices, normals, indices, fur_length=0.2, fur_angle=0, fur_density=0, M=poseMatrix(), material=None, primitive=GL_LINES, visible=True):
         """
         Initialises the model data
         """
         BaseModel.__init__(self, scene=scene, M=M,
                            primitive=primitive, visible=visible)
 
+        # Stores initial values to allow for paramaters to be changed back and forwards
         self.initial_vertices = vertices
         self.initial_normals = normals
         self.initial_indices = indices
 
+        # Fur parameters
         self.fur_length = fur_length
         self.fur_angle = fur_angle
         self.fur_density = fur_density
 
-        self.angle1 = angle1
-        self.angle2 = angle2
-
         self.initialise_vertices()
-        # self.indices = indices
 
         self.vertex_colors = None
+
+        # Sets the colour of the fur.
         self.material = Material(
             Ka=np.array([67/255, 45/255, 31/255], "f"),
             Kd=np.array([75/255, 63/255, 34/255], "f"),
@@ -45,12 +44,21 @@ class FurModel(BaseModel):
         self.bind()
 
     def initialise_vertices(self):
+        '''
+        Initialises the vertices that will be rendered according to the fur parameters
+        '''
+        # Create a deep copy so that the initials are not modified
         initial_vertices_copy = self.initial_vertices[:]
         initial_normals_copy = self.initial_normals[:]
         initial_indices_copy = self.initial_indices[:]
 
+        # Separates the fur into a short straight section followed by a longer angled section
+        fur_length_a = self.fur_length / 3
+        fur_length_b = self.fur_length * (2/3)
+
+        # Applies the fur densifier. Creates new vertices in the middle of triangles / quads for more fur
         for i in range(self.fur_density):
-            additional_vertices, additional_normals = densify_fur(
+            additional_vertices, additional_normals = self.densify_fur(
                 initial_vertices_copy, initial_normals_copy, initial_indices_copy)
 
             initial_vertices_copy = np.vstack(
@@ -58,64 +66,106 @@ class FurModel(BaseModel):
             initial_normals_copy = np.vstack(
                 (initial_normals_copy, additional_normals))
 
+        # Initialises the vertices and normals with 0's
         new_vertices = np.zeros_like(initial_vertices_copy)
         new_normals = np.zeros_like(initial_normals_copy)
 
         for vertex, normal in zip(initial_vertices_copy, initial_normals_copy):
+            # Add the current vertex
             new_vertices = np.vstack((new_vertices, vertex))
-            midpoint = vertex + (normal * self.fur_length)
+            # Find the midpoint of the hair by going from the current vertex in the normal direction by the fur length.
+            midpoint = vertex + (normal * fur_length_a)
+
+            # Initialise the endpoint of the fur. Angle it in the direction of the fur.
             endpoint = midpoint
 
-            endpoint[0] += (np.cos(self.fur_angle) * self.fur_length)
-            endpoint[1] += (np.sin(self.fur_angle) * self.fur_length)
-            endpoint[2] += self.fur_length
+            # Calculate the endpoint's actual location
+            endpoint[0] += (np.cos(self.fur_angle) *
+                            np.cos(self.fur_angle) * fur_length_b)
 
-            new_vertices = np.vstack(
-                (new_vertices, midpoint))
-            new_vertices = np.vstack(
-                (new_vertices, midpoint))
-            new_vertices = np.vstack(
-                (new_vertices, endpoint))
+            endpoint[1] += (np.sin(self.fur_angle) * fur_length_b)
+
+            endpoint[2] += fur_length_b * \
+                np.sin(self.fur_angle) * np.cos(self.fur_angle)
+
+            # Add the vertices to the buffer as required
+            new_vertices = np.vstack((new_vertices, midpoint))
+            new_vertices = np.vstack((new_vertices, midpoint))
+            new_vertices = np.vstack((new_vertices, endpoint))
+
             new_normals = np.vstack((new_normals, normal))
             new_normals = np.vstack((new_normals, normal))
 
         self.vertices = new_vertices
         self.normals = new_normals
 
+    def curvy_hair(self, vertex, normal, vertices, normals, fur_length):
+        '''
+        Deprecated fancy hair function. Created a smoother arc for the fur but was too resource intensive :(
+        :param vertex: The midpoint of the fur
+        :param normal: The normal vector of the vertex
+        :param vertices: List of vertices to append new fur points to
+        :param normals: List of normals to append new fur normals to
+        :param fur_length: The length of the fur
 
-def get_face_vertices(vertices, indices):
-    faces = []
-    for index in indices:
-        face = []
-        for i in index:
-            face.append(vertices[i])
-        faces.append(face)
-    return np.array(faces)
+        '''
+        a1 = 0
+        a2 = 0
+        while a1 < self.angle1:
+            while a2 < self.angle2:
+                vertices = np.vstack(
+                    (vertices, vertex))
+                new_point = vertex
+                new_point[0] += normal[0] * \
+                    fur_length * np.cos(a1) * np.cos(a2)
+                new_point[1] += normal[1] * fur_length * np.sin(a2)
+                new_point[2] += normal[2] * \
+                    fur_length * np.cos(a1) * np.sin(a2)
+                a1 += 15
+                a2 += 15
+                vertices = np.vstack(
+                    (vertices, new_point))
+        return vertices
 
+    def get_face_vertices(self, vertices, indices):
+        '''
+        From a list of vertices and indices, return a list of faces. 
+        :param vertices: The list of vertices to draw from
+        :param indices: The corresponding list of indices representing faces
+        '''
+        faces = []
+        for index in indices:
+            face = []
+            for i in index:
+                face.append(vertices[i])
+            faces.append(face)
+        return np.array(faces)
 
-def get_centroid(face):
-    return sum(face) / len(face)
+    def get_centroid(self, face):
+        '''
+        From a face list, return the centroid point.
+        :param face: The list of vectors representing a face
+        '''
+        return sum(face) / len(face)
 
+    def densify_fur(self, vertices, normals, indices):
+        '''
+        Returns a new list of vertex and normals. Each face is split into smaller segments at the center.
+        :param vertices: The list of indices to densify 
+        :param normals: The corresponding list of normals to densify
+        :param indices: The corresponding list of indices representing faces to densify
+        '''
+        vertex_faces = self.get_face_vertices(vertices, indices)
+        normal_faces = self.get_face_vertices(normals, indices)
 
-def add_to_buffer(buffer, old_face, new_centroid):
-    buffer = np.vstack((buffer, new_centroid))
-    return buffer
+        vertex_centroids = np.zeros_like(vertices)
+        normal_centroids = np.zeros_like(normals)
 
+        for index, vertex_face, normal_face in zip(indices, vertex_faces, normal_faces):
+            vertex_centroid = sum(vertex_face) / len(vertex_face)
+            normal_centroid = sum(normal_face) / len(normal_face)
 
-def densify_fur(vertices, normals, indices):
-    vertex_faces = get_face_vertices(vertices, indices)
-    normal_faces = get_face_vertices(normals, indices)
+            vertex_centroids = np.vstack((vertex_centroids, vertex_centroid))
+            normal_centroids = np.vstack((normal_centroids, normal_centroid))
 
-    vertex_centroids = np.zeros_like(vertices)
-    normal_centroids = np.zeros_like(normals)
-
-    for index, vertex_face, normal_face in zip(indices, vertex_faces, normal_faces):
-        vertex_centroid = sum(vertex_face) / len(vertex_face)
-        normal_centroid = sum(normal_face) / len(normal_face)
-
-        vertex_centroids = add_to_buffer(
-            vertex_centroids, vertex_face, vertex_centroid)
-
-        normal_centroids = add_to_buffer(
-            normal_centroids, normal_face, normal_centroid)
-    return vertex_centroids, normal_centroids
+        return vertex_centroids, normal_centroids
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Graphics Coursework
- Coursework for ECM3423 Graphics
+
+Coursework for ECM3423 Graphics
+
+# Getting Started
+
+To get started, we need to fetch the dependencies for this project. Using a virtual environment is advisable:
+
+1.  Navigate to the source code directory
+2.  Run the command `python -m venv .venv`
+3.  Run .venv\scripts\activate.bat
+4.  Run the command `pip install -r requirements.txt`
+5.  Run the main file by entering `python main.py`
+
+# Controlling the world
+
+As described in the coursework specification, the world can be interacted with in the following ways:
+
+- 'l' - increase fur length
+- 'k' - decrease fur length
+- 'm' - increase fur density
+- 'n' - decrease fur density
+- Arrow Keys - rotations
+- Mouse - translations
+- 'b' move fur in random direction
+
+## Switching between rabbit and torus
+
+In order to switch which object is being shown, navigate to `main.py` and comment / uncomment the lines 88-97 and 99-109.
